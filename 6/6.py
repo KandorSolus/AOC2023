8526fb8a9250855e17a7b8614eb9c57ef730b51e
Jesper Junker <jesper.junker@hbt.de>
2023-12-06 12:49:33 +0100

    use zip

--- 6/6.py
+++ 6/6.py
@@ -1,24 +1,20 @@
 import math
-
+import numpy
 f = open("6.txt", "r")
 lines = f.readlines()
 
-def solve(t, d):
-    sol = t/2-math.sqrt(t**2/4-d)
+def solve(race):
+    sol = race[0]/2-math.sqrt(race[0]**2/4-race[1])
     sol = sol + 1 if math.ceil(sol) == sol else math.ceil(sol)
-    return (t - 2*sol + 1)
+    return (race[0] - 2*sol + 1)
 
 #part1
 times = list(map(int, lines[0].split(":")[1].split()))
 distances = list(map(int, lines[1].split(":")[1].split()))
-sols = 1
-for i in range(len(times)):
-    t = times[i]
-    d = distances[i]
-    sols *= solve(t, d)
-print(sols)
+print(numpy.prod(list(map(solve, zip(times, distances)))))
 
 #part2
 t = int("".join(map(str, times)))
 d = int("".join(map(str, distances)))
-print(solve(t,d))
+race = (t, d)
+print(solve(race))
