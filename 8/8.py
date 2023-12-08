0bc2473748d7f75c6d007a8983cce05fdd94f530
Jesper Junker <jesper.junker@hbt.de>
2023-12-08 10:16:30 +0100

    use lcm in part2 not working yet

--- 8/8.py
+++ 8/8.py
@@ -1,19 +1,18 @@
-import collections
+import numpy as np
 f = open("8.txt", "r")
 dicts = {}
 lines = f.readlines()
 algorithm = lines[0].strip()
 lines = lines[1:]
-#[[dicts[x[0].strip()] = tuple(x[1].strip(" ()").split(", ")) for x in line.split("=")] for line in lines]
+
 for line in lines:
     if "=" in line:
         line = line.split("=")
         dicts[line[0].strip()] = tuple(line[1].strip(" ()\n").split(", "))
 keys = list(filter(lambda x: x.endswith("A"), dicts.keys()))
-steps = 0
-def step(key):
-    global steps
-    if algorithm[steps % len(algorithm)] == "L":
+steps = []
+def step(key, stept):
+    if algorithm[stept % len(algorithm)] == "L":
         key = dicts[key][0]
     else:
         key = dicts[key][1]
@@ -21,9 +20,11 @@ def step(key):
 
 finished = False
 
-while not finished:
-    keys = list(map(step, keys))
-    steps += 1
-    fnished = all(list(map(lambda x: x.endswith("Z"), keys)))
+for key in keys:
+    stept = 0
+    while not key.endswith("Z"):
+        key = step(key, stept)
+        stept += 1
+    steps.append(stept)
 
-print(steps)
+print(np.lcm.reduce(steps))
